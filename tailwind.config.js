2e08bc9eaf350ea98ae10c84933edb4e5086e291
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-08-04 12:18:37 +0800

    :package: feat: install flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./src/component/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+    "./node_modules/flowbite/**/*.jsx",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
