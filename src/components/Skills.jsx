5ab105a6453d28e93619fb92eb105cf0040a29df
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-08-30 20:40:55 +0800

    :sparkle: add hard skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,32 @@
 import React from "react";
+import HardSkills from "./HardSkills";
 
 const Skills = () => {
   return (
-    <div className="md:min-w-screen min-w-screen md:min-h-screen min-h-screen flex">
-      {/* soft skills */}
-      <div className="bg-pastelBeige w-full"></div>
+    <div className="md:min-w-screen min-w-screen md:min-h-screen min-h-screen">
+      <div className="flex">
+        {/* technical skills */}
+        <div className="bg-pastelBeige w-full">
+          <h1 className="name w-fit py-2 px-4 shadow-md bg-dirtyWhite rounded-lg md:text-3xl md:mx-auto ml-6 mt-20 mb-6 text-gray-900 text-center">
+            HARD Skills
+          </h1>
 
-      {/* diagonal separator */}
-      <div className="bg-white md:w-0 w-2/4 md:border-l-[20rem] border-l-[7rem] border-l-pastelBeige md:border-b-[39rem] border-b-[49rem] border-b-dirtyWhite"></div>
+          <div className="md:flex">
+            {/* skill set */}
+            <HardSkills />
+          </div>
+        </div>
 
-      {/* technical skills */}
-      <div className="bg-dirtyWhite w-full"></div>
+        {/* diagonal separator */}
+        <div className="bg-white md:border-l-[12rem] border-l-[4rem] border-l-pastelBeige md:border-b-[39rem] border-b-[49rem] border-b-dirtyWhite"></div>
+
+        {/* soft skills */}
+        <div className="bg-dirtyWhite w-full relative">
+          <h1 className="name text-center w-fit py-2 px-4 shadow-md bg-pastelBeige rounded-lg md:text-3xl md:mx-auto mx-6 my-8 md:my-20 text-black">
+            SOFT Skills
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
