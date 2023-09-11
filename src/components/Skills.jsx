59d435d6a6bb770c9c3d13392a685ac2514d6f6a
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-09-11 13:45:42 +0800

    :lipstick: feat: change color palette

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
     <div className="md:min-w-screen md:min-h-screen min-h-screen">
       <div className="flex">
         {/* technical skills */}
-        <div className="bg-pastelBeige w-full">
+        <div className="bg-peach w-full">
           <h1 className="name w-fit py-2 px-4 shadow-md bg-dirtyWhite rounded-lg md:text-3xl mx-auto mt-10 mb-6 text-gray-900 text-center">
             HARD Skills
           </h1>
@@ -18,11 +18,11 @@ const Skills = () => {
         </div>
 
         {/* diagonal separator */}
-        <div className="bg-white md:border-l-[12rem] border-l-[4rem] border-l-pastelBeige md:border-b-[39rem] border-b-[49rem] border-b-dirtyWhite"></div>
+        <div className="bg-white md:border-l-[12rem] border-l-[4rem] border-l-peach md:border-b-[39rem] border-b-[49rem] border-b-dirtyWhite"></div>
 
         {/* soft skills */}
         <div className="bg-dirtyWhite w-full relative">
-          <h1 className="name text-center w-fit py-2 px-4 shadow-md bg-pastelBeige rounded-lg md:text-3xl md:mx-auto mx-6 my-8 md:my-20 text-black">
+          <h1 className="name text-center w-fit py-2 px-4 shadow-md bg-peach rounded-lg md:text-3xl md:mx-auto mx-6 my-8 md:my-20 text-black">
             SOFT Skills
           </h1>
         </div>
