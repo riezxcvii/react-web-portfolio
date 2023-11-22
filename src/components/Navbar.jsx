679f8a988c6c2c80d0375d4e4bc478a0bf10e4fe
rieza-ix <rizmarezlo@gmail.com>
2023-11-22 12:15:39 +0800

    :lipstick: fix: add 'Hire Me' button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     setDrawerOpen(!isDrawerOpen);
   };
 
-  // set array value for projects and call it in the list
+  // Set array value for navigation links and call it in the list
   const [navigation] = useState([
     { page: "Home", link: "", id: 1 },
     { page: "Skills", link: "", id: 2 },
@@ -23,31 +23,39 @@ const Navbar = () => {
           <span className="title  self-center text-2xl font-semibold whitespace-nowrap text-white">
             Rieza Banquillo
           </span>
-          <button
-            onClick={toggleDrawer}
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm hover:text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-expanded={isDrawerOpen ? "true" : "false"}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5 z-40"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              className="text-[#5f65da] bg-white shadow-lg focus:ring-2 focus:outline-none focus:ring-[#5f65da] font-medium rounded-lg text-sm px-4 py-2 text-center"
             >
-              <path
-                stroke="white"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
+              Hire Me
+            </button>
+            <button
+              onClick={toggleDrawer}
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm hover:text-black rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              aria-expanded={isDrawerOpen ? "true" : "false"}
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5 z-40"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+          </div>
 
-          {/* Navbar links in mobile view */}
+          {/* Navigation links in mobile view */}
           <div
             id="drawer-right"
             className={`fixed top-0 right-0 z-50 h-screen p-4 overflow-y-auto transition-transform ${
@@ -94,7 +102,7 @@ const Navbar = () => {
             </ul>
           </div>
 
-          {/* Navbar links in desktop view */}
+          {/* Navigation links in desktop view */}
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-[#6D72DF] md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-[#6D72DF] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {navigation.map((navLink) => (
